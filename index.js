6ecbf6f4091029d71c34a89727cae8eceb0b719e
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-17 12:17:38 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = process.env.PORT || 3000;
